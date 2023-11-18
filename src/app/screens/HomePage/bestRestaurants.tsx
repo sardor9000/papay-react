07451a604b9c08868ef8cafecf088d62c1df06af
Sardor <usok201802@gmail.com>
2023-11-18 14:42:35 +0900

    fix: modify homepage css styles

--- src/app/screens/HomePage/bestRestaurants.tsx
+++ src/app/screens/HomePage/bestRestaurants.tsx
@@ -11,7 +11,7 @@ export function BestRestaurants() {
         <div className='best_restaurant_frame'>
             <img src={"icons/line_group.svg"}
             style={{position: "absolute", left: "6%", transform: "rotate(0deg)"}} />
-            <Container sx={{ mt: "153px" }} >
+            <Container sx={{ paddingTop: "153px" }} >
                 <Stack flexDirection={"column"} alignItems={"center"}>
                     <Box className="category_title">Zo'r Restaurantlar</Box>
                     <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
