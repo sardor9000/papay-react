96a97db494cc487411574d2a0804b3276d4576a2
Sardor <usok201802@gmail.com>
2023-11-20 22:16:28 +0900

    feat: homepage related publishing finish

--- src/app/screens/HomePage/recommendation.tsx
+++ src/app/screens/HomePage/recommendation.tsx
@@ -1,11 +1,144 @@
-import { Container } from '@mui/material';
+import { Avatar, Box, Container, Stack } from '@mui/material';
 import React from 'react';
 
 
 export function Recommendations() {
     return (
-        <div className='top_restaurant_frame'>
-            <Container>Recommendation</Container>
+        <div className='top_article_frame'>
+            <Container
+                maxWidth="lg"
+                sx={{ mb: "50px", mt: "60px" }}
+                style={{ position: "relative" }}
+            >
+                <Stack
+                    flexDirection={"column"}
+                    alignItems={"center"}
+                    sx={{ mt: "45px" }}
+                >
+                    <Box className="category_title">Tafsiya qilingan maqolalar</Box>
+                    <Stack className={"article_main"} flexDirection={"row"}>
+                        <Stack className={"article_container"}>
+                            <Box className={"article_category"}>Ko'p ko'rilgan</Box>
+
+                            <Stack className={"article_box"}>
+                                <Box
+                                    className={"article_img"}
+                                    sx={{
+                                        backgroundImage: `url(
+                                            'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D')`
+                                    }}
+                                ></Box>
+                                <Box className={"article_info"}>
+                                    <Box className={"article_main_info"}>
+                                        <div className={"article_author"}>
+                                            <Avatar
+                                                alt='Author_photo'
+                                                src={"/auth/default_user.svg"}
+                                                sx={{ width: "35px", height: "35px" }}
+                                            />
+                                            <span className={"author_username"}>Sardor</span>
+                                        </div>
+                                        <span className={"article_title"}>
+                                            Eng qiziqarli va shirin taomlar
+                                        </span>
+                                        <p className={"article_desc"}></p>
+                                    </Box>
+                                </Box>
+                            </Stack>
+
+                            <Stack className={"article_box"}>
+                                <Box
+                                    className={"article_img"}
+                                    sx={{
+                                        backgroundImage: `url(
+                                            'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D')`
+                                    }}
+                                ></Box>
+                                <Box className={"article_info"}>
+                                    <Box className={"article_main_info"}>
+                                        <div className={"article_author"}>
+                                            <Avatar
+                                                alt='Author_photo'
+                                                src={"/auth/default_user.svg"}
+                                                sx={{ width: "35px", height: "35px" }}
+                                            />
+                                            <span className={"author_username"}>Sardor</span>
+                                        </div>
+                                        <span className={"article_title"}>
+                                            Eng qiziqarli va shirin taomlar
+                                        </span>
+                                        <p className={"article_desc"}></p>
+                                    </Box>
+                                </Box>
+                            </Stack>
+
+                            <Box className={"article_category"}>Ko'p yoqtirilgan</Box>
+
+                            <Stack className={"article_box"}>
+                                <Box
+                                    className={"article_img"}
+                                    sx={{
+                                        backgroundImage: `url(
+                                            'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D')`
+                                    }}
+                                ></Box>
+                                <Box className={"article_info"}>
+                                    <Box className={"article_main_info"}>
+                                        <div className={"article_author"}>
+                                            <Avatar
+                                                alt='Author_photo'
+                                                src={"/auth/default_user.svg"}
+                                                sx={{ width: "35px", height: "35px" }}
+                                            />
+                                            <span className={"author_username"}>Neo</span>
+                                        </div>
+                                        <span className={"article_title"}>
+                                            Jizzax somsasining mazasi o'zgacha
+                                        </span>
+                                        <p className={"article_desc"}></p>
+                                    </Box>
+                                </Box>
+                            </Stack>
+
+                            <Stack className={"article_box"}>
+                                <Box
+                                    className={"article_img"}
+                                    sx={{
+                                        backgroundImage: `url(
+                                            'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D')`
+                                    }}
+                                ></Box>
+                                <Box className={"article_info"}>
+                                    <Box className={"article_main_info"}>
+                                        <div className={"article_author"}>
+                                            <Avatar
+                                                alt='Author_photo'
+                                                src={"/auth/default_user.svg"}
+                                                sx={{ width: "35px", height: "35px" }}
+                                            />
+                                            <span className={"author_username"}>Neo</span>
+                                        </div>
+                                        <span className={"article_title"}>
+                                        Jizzax somsasining mazasi o'zgacha
+                                        </span>
+                                        <p className={"article_desc"}></p>
+                                    </Box>
+                                </Box>
+                            </Stack>
+                        </Stack>
+
+                        <Stack className={"article_container"}>
+                            <Box className={"article_category"}>Mashhurlar</Box>
+                            <Box className={"article_news"}>
+                                <h1 style={{color: "orange"}}>TViewer</h1>
+                            </Box>
+                            <Box className={"article_news"}>
+                                <h1 style={{color: "orange"}}>TViewer</h1>
+                            </Box>
+                        </Stack>
+                    </Stack>
+                </Stack>
+            </Container>
         </div>
     )
 }
