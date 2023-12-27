a7bee7aeacb32bc0be3c975be1698b101f4efa8e
Sardor <usok201802@gmail.com>
2023-12-27 18:37:41 +0900

    feat: develop signup authentication process

--- src/app/apiServices/memberApiService.ts
+++ src/app/apiServices/memberApiService.ts
@@ -29,6 +29,25 @@ class MemberApiService {
             throw err;
         }
     }
+
+    public async signupRequest(signup_data: any) {
+        try {
+            const result = await axios.post(this.path + "/signup", signup_data,
+                {
+                    withCredentials: true
+                });
+            console.log("state:", result.data.sate);
+            assert.ok(result.data, Definer.general_err2);
+            assert.ok(result?.data.state != "fail", result?.data.message);
+
+            const member: Member = result.data.data;
+            localStorage.setItem("member_data", JSON.stringify(member));
+            return member
+        } catch (err: any) {
+            console.log(`ERROR:: signupRequest ${err.message}`);
+            throw err;
+        }
+    }
 }
 
 export default MemberApiService;
--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -50,6 +50,29 @@ export default function AuthenticationModal(props: any) {
     const handlePassword = (e: any) => { 
         mb_password = e.target.value;
     };
+  const handleSignupRequest = async () => {
+    try {
+      const is_fulfilled = mb_nick != "" && mb_password != "" && mb_phone;
+      assert.ok(is_fulfilled, Definer.input_err1);
+
+      const signup_data = {
+        mb_nick: mb_nick,
+        mb_phone: mb_phone,
+        mb_password: mb_password
+      };
+      
+      const memberApiService = new MemberApiService();
+      await memberApiService.signupRequest(signup_data);
+
+      props.handleSignUpClose();
+      window.location.reload();
+
+    } catch (err) {
+      console.log(err);
+      props.handleLoginClose();
+      sweetErrorHandling(err).then();
+    }
+  }
 
     const handleLoginRequest = async () => {
         try {
@@ -58,14 +81,14 @@ export default function AuthenticationModal(props: any) {
 
             const login_data = {
                 mb_nick: mb_nick,
-                mb_password: mb_password
+                mb_password: mb_password 
             };
 
           const memberApiService = new MemberApiService();
           await memberApiService.loginRequest(login_data);
 
           props.handleLoginClose();
-          window.location.reload;
+          window.location.reload();
         } catch (err) {
             console.log(err);
             props.handleLoginClose();
@@ -101,27 +124,27 @@ export default function AuthenticationModal(props: any) {
             <Stack sx={{ marginLeft: "69px", alignItems: "center" }}>
               <h2>SignUp Form</h2>
               <TextField
-                // onChange={}
+                onChange={handleUsername}
                 sx={{ marginTop: "7px" }}
                 id="outlined-basic"
                 label="username"
                 variant="outlined"
               />
               <TextField
-                // onChange={}
+                onChange={handlePhone}
                 sx={{ my: "17px" }}
                 id="outlined-basic"
                 label="phone number"
                 variant="outlined"
               />
               <TextField
-                // onChange={}
+                onChange={handlePassword}
                 id="outlined-basic"
                 label="password"
                 variant="outlined"
               />
               <Fab
-                // onClick={}
+                onClick={handleSignupRequest}
                 sx={{ marginTop: "30px", width: "120px" }}
                 variant="extended"
                 color="primary"
