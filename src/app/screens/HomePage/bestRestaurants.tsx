6b5d459edaf4ad75b32c67d687b8aac2c3ed157a
Sardor <usok201802@gmail.com>
2023-12-30 12:25:46 +0900

    feat: member like best restaurants logic creation

--- src/app/screens/HomePage/bestRestaurants.tsx
+++ src/app/screens/HomePage/bestRestaurants.tsx
@@ -1,168 +1,227 @@
+import React, { useRef } from "react";
+import { Box, Button, Container, Stack } from "@mui/material";
+import {
+  AspectRatio,
+  Card,
+  CardOverflow,
+  CssVarsProvider,
+  IconButton,
+  Link,
+  Typography,
+} from "@mui/joy";
+import Favorite from "@mui/icons-material/Favorite";
+import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
+import CallIcon from "@mui/icons-material/Call";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { SentimentNeutral } from "@mui/icons-material";
+import assert from "assert";
+import MemberApiService from "../../apiServices/memberApiService";
+import { Definer } from "../../../lib/Definer";
+import {
+  sweetErrorHandling,
+  sweetTopSmallSuccessAlert,
+} from "../../../lib/sweetAlert";
+import { useHistory } from "react-router-dom";
 
+//Redux
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveBestRestaurants } from "../../screens/HomePage/selector";
+import { Restaurant } from "../../../types/user";
+import { serverApi } from "../../../lib/config";
 
-import React from 'react';
-import { Favorite, LocationOnRounded, Visibility } from '@mui/icons-material';
-import { AspectRatio, Card, CardOverflow, CssVarsProvider, IconButton, Typography, Link } from '@mui/joy';
-import { Box, Button, Container, Stack } from '@mui/material';
-import CallIcon from "@mui/icons-material/Call"
-// REDUX
-import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
-import { setBestRestaurants } from "../../screens/HomePage/slice";
-import {retrieveBestRestaurants } from "../../screens/HomePage/selector";
-import { Restaurant } from '../../../types/user';
-import { serverApi } from '../../../lib/config';
-
-// REDUX SELECTOR
-const bestRestaurantRetriever = createSelector(
-    retrieveBestRestaurants,
-    (bestRestaurants) => ({
-        bestRestaurants
-    })
+//** Redux Selector */
+const bestRestaurantsRetriever = createSelector(
+  retrieveBestRestaurants,
+  (bestRestaurants) => ({
+    bestRestaurants,
+  })
 );
 
 export function BestRestaurants() {
-    // INITIALIZATION
-    const { bestRestaurants } = useSelector(bestRestaurantRetriever);
-    return (
-        <div className='best_restaurant_frame'>
-            <img src={"icons/line_group.svg"}
-            style={{position: "absolute", left: "6%", transform: "rotate(0deg)"}} />
-            <Container sx={{ paddingTop: "153px" }} >
-                <Stack flexDirection={"column"} alignItems={"center"}>
-                    <Box className="category_title">Zo'r Restaurantlar</Box>
-                    <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
-                   
-                        {bestRestaurants.map((ele: Restaurant) => {
-                            const image_path = `${serverApi}/${ele.mb_image}`;
-                                
-                                return (
-                                    <CssVarsProvider>  
-                                    <Card
-                                variant="outlined"
-                                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
-                            >
-                                <CardOverflow>
-                                    <AspectRatio ratio="1">
-                                        <img src={image_path} />
-                                    </AspectRatio>
-                                    <IconButton
-                                        aria-label='Like minimal photography'
-                                        size="md"
-                                        variant="solid"
-                                        color="neutral"
-                                        sx={{
-                                            position: "absolute",
-                                            zIndex: 2,
-                                            borderRadius: "50%",
-                                            right: "rem",
-                                            bottom: 0,
-                                            transform: "translateY(50%)",
-                                            color: "rgba(0, 0 , 0,.4)"
-                                        }}
-                                        onClick={(e) => {
-                                            e.stopPropagation();
-                                          }}
-                                    >
-                                          <Favorite
-                                            style={{
-                                                            fill:
-                                                    ele?.me_liked && ele?.me_liked[0]?.my_favorite
-                                                        ? "red"
-                                                        : "white"
-                                        
-                                        }}/>
+  // Initializations
+  const { bestRestaurants } = useSelector(bestRestaurantsRetriever);
+  const history = useHistory();
+  const refs: any = useRef([]);
 
-                                    </IconButton>
-                                </CardOverflow>
-                                <Typography level="h2" sx={{fontSize: "md", mt: 2}}>
-                                    {ele.mb_nick}
-                                </Typography>
-                                <Typography level="body-sm" sx={{mt: 0.5, mb: 2}}>
-                                    <Link
-                                        href=""
-                                        startDecorator={<LocationOnRounded />}
-                                        textColor="neutral.700"
-                                    >
-                                    {ele.mb_address}
-                                        
-                                    </Link>
-                                </Typography>
-                                <Typography level="body-sm" sx={{mt: 0.5, mb: 2}}>
-                                    <Link
-                                        href=""
-                                        startDecorator={<CallIcon />}
-                                        textColor="neutral.700"
-                                    >
-                                    {ele.mb_phone}
-                                       
-                                    </Link>
-                                </Typography>
-                                <CardOverflow
-                                    variant='soft'
-                                    sx={{
-                                        display: "flex",
-                                        gap: 1.5,
-                                        py: 1.5,
-                                        px: "var(--Card-padding)",
-                                        borderTop: "1px solid",
-                                        borderColor: "neutral.outlinedBorder",
-                                        bgcolor: "background.level1",
-                                        flexDirection: "row",
+  /** Handlers */
+  const chosenRestaurantHandler = (id: string) => {
+    history.push(`/restaurant/${id}`);
+  };
+  const goRestaurantHandler = () => history.push("/restaurant");
+  const targetLikeBest = async (e: any, id: string) => {
+    try {
+      assert.ok(localStorage.getItem("member_data"), Definer.auth_err1);
 
+      const memberService = new MemberApiService(),
+        like_result: any = await memberService.memberLikeTarget({
+          like_ref_id: id,
+          group_type: "member",
+        });
+      assert.ok(like_result, Definer.general_err1);
 
-                                    }}
-                                >
-                                    <Typography
-                                        level="body-sm"
-                                        sx={{
-                                            fontWeight: "md",
-                                            color: "text.secondary",
-                                            alignItems: "center",
-                                            display: "flex",
-                                        }}
-                                    >
-                                        {ele.mb_views}
-                                        <Visibility sx={{ fontSize: 20, marginLeft: "5px" }} />
-                                    </Typography>
-                                    <Box sx={{ width: 2, bgcolor: "divider" }} />
-                                    <Typography
-                                        level="body-sm"
-                                        sx={{
-                                            fontWeight: "md",
-                                            color: "text.secondary",
-                                            alignItems: "center",
-                                            display: "flex",
-                                        }}
-                                    >
-                                   <div>{ele.mb_likes }</div>
-                                        <Favorite sx={{fontSize: 20, marginLeft: "5px"}} />
-                                        
+      if (like_result.like_status > 0) {
+        e.target.style.fill = "red";
+        refs.current[like_result.like_ref_id].innerHTML++;
+      } else {
+        e.target.style.fill = "white";
+        refs.current[like_result.like_ref_id].innerHTML--;
+      }
 
-                                    </Typography>
+      await sweetTopSmallSuccessAlert("success", 700, false);
+    } catch (err: any) {
+      console.log("targetLikeBest, ERROR:", err);
+      sweetErrorHandling(err).then();
+    }
+  };
 
-                                </CardOverflow>
-                            </Card>
-                                    </CssVarsProvider>
+  return (
+    <div className="best_restaurant_frame">
+      <img
+        src={"./icons/line_group.svg"}
+        style={{
+          position: "absolute",
+          left: "6%",
+          transform: "rotate(90deg)",
+        }}
+      />
+      <Container sx={{ paddingTop: "153px" }}>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Box className="category_title">Best Restaurants</Box>
+          <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
+            {bestRestaurants.map((ele: Restaurant) => {
+              const image_path = `${serverApi}/${ele.mb_image}`;
+              return (
+                <CssVarsProvider>
+                  <Card
+                    onClick={() => chosenRestaurantHandler(ele._id)}
+                    variant="outlined"
+                    sx={{
+                      minHeight: 483,
+                      minWidth: 320,
+                      mr: "35px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <CardOverflow>
+                      <AspectRatio ratio="1">
+                        <img src={image_path} />
+                      </AspectRatio>
+                      <IconButton
+                        aria-label="Like minimal photography"
+                        size="md"
+                        variant="solid"
+                        color="danger"
+                        sx={{
+                          position: "absolute",
+                          zIndex: 2,
+                          borderRadius: "50%",
+                          right: "1rem",
+                          bottom: 0,
+                          transform: "translateY(50%)",
+                          color: "rgba(0,0,0,.4)",
+                        }}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                        }}
+                      >
+                        <Favorite
+                          onClick={(e) => targetLikeBest(e, ele._id)}
+                          style={{
+                            fill:
+                              ele?.me_liked && ele?.me_liked[0]?.my_favorite
+                                ? "red"
+                                : "white",
+                          }}
+                        />
+                      </IconButton>
+                    </CardOverflow>
+                    <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
+                      {ele.mb_nick}
+                    </Typography>
+                    <Typography level="body-sm" sx={{ mt: 0.5, mb: 2 }}>
+                      <Link
+                        href=""
+                        startDecorator={<LocationOnRoundedIcon />}
+                        textColor="neutral.700"
+                      >
+                        Tashkent, Kosmonavtlar 1-12
+                      </Link>
+                    </Typography>
+                    <Typography level="body-sm" sx={{ mt: 0.5, mb: 2 }}>
+                      <Link
+                        href=""
+                        startDecorator={<CallIcon />}
+                        textColor="neutral.700"
+                      >
+                        {ele.mb_phone}
+                      </Link>
+                    </Typography>
+                    <CardOverflow
+                      sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: 1.5,
+                        py: 1.5,
+                        borderTop: "1px solid",
+                        borderColor: "neutral.outlineBorder",
+                        bgcolor: "background.level1",
+                      }}
+                    >
+                      <Typography
+                        level="body-xs"
+                        sx={{
+                          fontweight: "md",
+                          color: "text.secondary",
+                          alignItems: "center",
+                          display: "flex",
+                        }}
+                      >
+                        {ele.mb_views}
+                        <VisibilityIcon
+                          sx={{ fontSize: 20, marginLeft: "5px" }}
+                        />
+                      </Typography>
+                      <Box sx={{ width: 2, bgcolor: "divider" }} />
+                      <Typography
+                        level="body-xs"
+                        sx={{
+                          fontweight: "md",
+                          color: "text.secondary",
+                          alignItems: "center",
+                          display: "flex",
+                        }}
+                      >
+                        <div
+                          ref={(element) => {
+                            refs.current[ele._id] = element;
+                          }}
+                        >
+                          {ele.mb_likes}
+                        </div>
+                        <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
+                      </Typography>
+                    </CardOverflow>
+                  </Card>
+                </CssVarsProvider>
+              );
+            })}
+          </Stack>
 
-                                )
-                            })}
-                      
-                      
-                    </Stack>
-
-                    <Stack
-                        flexDirection={"row"}
-                        justifyContent={'flex-end'}
-                        style={{ width: "100%", marginTop: "16px" }}>
-                        <Button
-                            style={{ background: "#1976d2", color: "#ffffff" }}>
-                            Barchasini Ko'rish
-                        </Button>
-                    </Stack>
-
-                </Stack>
-            </Container>
-        </div>
-    )
-} 
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"flex-end"}
+            style={{ width: "100%", marginTop: "16px" }}
+          >
+            <Button
+              style={{ background: "#1976D2", color: "#FFFFFF" }}
+              onClick={goRestaurantHandler}
+            >
+              Barchasini ko'rish
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
